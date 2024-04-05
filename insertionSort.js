859e982143ff6de7cc0120b1e7a2012d4c569665
unaisek <mohammedunaiseknkt@gmail.com>
2024-04-05 14:03:48 +0530

    insertion sort method

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for(let i=1; i<arr.length ; i++){
+    let temp = arr[i];
+    let j = i - 1;
+    while( j>= 0 && arr[j] > temp){
+      arr[j+1] = arr[j];
+      j--
+    }
+    arr[j+1] = temp
+  }
+  return arr
+}
+
+const arr = [ 10, 4, 29, 22, 45, 46, 23, 3];
+console.log(insertionSort(arr));
