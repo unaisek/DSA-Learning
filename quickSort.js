5a5358baba4ae23b42de4c86b27816be38cd7e2e
unaisek <mohammedunaiseknkt@gmail.com>
2024-04-06 09:39:24 +0530

    quick sort algoithm

---
+++ quickSort.js
@@ -0,0 +1,22 @@
+function quickSort(arr){
+  if(arr.length < 2){
+    return arr
+  }
+  let pivot = arr[0];
+  let left = [];
+  let right = []
+
+  for(let i=1; i<arr.length; i++){
+    if(arr[i] < pivot){
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+const arr =[20, 45, 34, 26, 76, 5, 22];
+
+console.log(quickSort(arr));
