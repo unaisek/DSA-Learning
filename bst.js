71c98db4c3af3bbe148e44b6e00b5287ee570fd3
unaisek <mohammedunaiseknkt@gmail.com>
2024-04-12 10:38:37 +0530

    deleting a node from bst implemented

--- bst.js
+++ bst.js
@@ -36,11 +36,44 @@ class Bst {
     }
   }
 
+  delete(value){
+    this.root = this.deleteNode(this.root, value)
+  }
+
+  deleteNode(root, value){
+    if(value < root.value){
+      root.left = this.deleteNode(root.left, value)
+    } else if(value > root.value) {
+      root.right = this.deleteNode(root.right, value)
+    } else {
+      if(!root.left && !root.right){
+        return null;
+      } else if(!root.left){
+        return root.right;
+      } else if(!root.right){
+        return root.left;
+      }
+
+      root.value = this.min(root.right);
+      console.log(root.value,"min");
+      root.right = this.deleteNode(root.right, root.value)
+    }
+    return root
+  }
+
   inOrder(root){
     if(root){
       this.inOrder(root.left);
       console.log(root.value);
       this.inOrder(root.right);
+    }
+  }
+
+  min(root){
+    if(!root.left){
+      return root.value
+    } else {
+      return this.min(root.left)
     }
   }
 
@@ -54,5 +87,7 @@ bst.insert(30);
 bst.insert(10);
 bst.insert(50);
 bst.insert(70);
+
+bst.delete(40)
 
 bst.inOrder(bst.root)
