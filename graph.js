ee321bd59d61f105583b98a7064b3d23d084df3c
unaisek <mohammedunaiseknkt@gmail.com>
2024-04-16 12:08:20 +0530

    graph cyclic detection

--- graph.js
+++ graph.js
@@ -27,11 +27,38 @@ class Graph{
       console.log(`${vertex} -> ${neighbours.join(", ")}`);
     }
   }
+
+  hasCyclic(){
+    let visited = new Set();
+    for(let vertex in this.adjacenyList){
+      if(!visited.has(vertex)){
+        if(this.hasCyclicUtil(vertex, visited, null)){
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  hasCyclicUtil(vertex, visited, parent){
+    visited.add(vertex);
+    for(let neighbour of this.adjacenyList[vertex] ){
+      if(!visited.has(neighbour)){
+        if(this.hasCyclicUtil(neighbour, visited, vertex )){
+          return true
+        } else if(parent !== neighbour){
+          return true
+        }
+      }
+    }
+    return false
+  }
 }
 
 const graph = new Graph();
-graph.addEdges(10, 20);
+// graph.addEdges(10, 20);
 graph.addVertex(50);
 graph.addVertex(100);
-graph.addEdges(10, 100);
+// graph.addEdges(10, 100);
 graph.print()
+console.log(graph.hasCyclic());
